fc58722b22e272de149e07adb41eecf9f1ec870a
Abraham <abradiazmediavilla@gmail.com>
2022-10-24 18:44:44 +0200

    Disparos WIP

---
+++ Hueguito/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    private Vector3 mousePos;
+    private Camera mainCam;
+    private Rigidbody2D rb;
+    public float force, destroyDelay = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        rb = GetComponent<Rigidbody2D>();
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = mousePos - transform.position;
+        Vector3 rotation = transform.position - mousePos;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+        float rot = Mathf.Atan2(rotation.x, rotation.y) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
+        WaitAndDestroy();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void WaitAndDestroy()
+    {
+        Destroy(this.gameObject, destroyDelay);
+    }
+
+}
